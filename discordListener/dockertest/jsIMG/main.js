ee0c5480b7f8a3a38cf477c56bd2a4dbaf6ae354
Aidan Murray <aidanm0121@gmail.com>
2020-06-08 19:57:39 -0400

    express server

--- discordListener/dockertest/jsIMG/main.js
+++ discordListener/dockertest/jsIMG/main.js
@@ -1,1 +1,11 @@
-console.log("Hello World from JS")
+console.log("Hello World from JS");
+
+const express = require("express");
+const app = express();
+const port = 8080
+
+app.get("/", function (req, res) {
+    res.send("Hello World");
+})
+
+app.listen(port, () => console.log("we up, port: "+port));
